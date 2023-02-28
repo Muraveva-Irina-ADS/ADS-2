a607ff14f50105a8acf29e6db0f3ffc400b50d00
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-02-28 20:50:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    uint16_t factor = 1;
+    uint64_t factor = 1;
     for (int i = 2; i <= n; i++)
         factor *= i;
     return factor;
