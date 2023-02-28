8d902e5fc98a426c4889f1075bdf2b302f66806d
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-02-28 18:51:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,50 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double st = 1;
+    for (int i = 1; i <= n; i++)
+        st *= value;
+    return st;
 }
 
 uint64_t fact(uint16_t n) {
-
+    int factor = 1;
+    for (int i = 2; i <= n; i++)
+        factor *= i;
+    return factor;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double sum = 0;
+    for (int i = 0; i <= count; i++)
+        sum += pown(x, i) / fact(i);
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+    int znac = 0;
+    double sum = 0;
+    for (int i = 1; i <= count * 2-1; i += 2) {
+        znac++;
+        if (znac % 2 == 0) sum -= pown(x, i) / fact(i);
+        else
+            sum += pown(x, i) / fact(i);
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-
+    int znac = 0;
+    double sum = 0;
+    for (int i = 0; i <= count * 2 - 2; i += 2) {
+        znac++;
+        if (znac % 2 == 0) sum -= pown(x, i) / fact(i);
+        else
+            sum += pown(x, i) / fact(i);
+    }
+    return sum;
 }
